053603f1c59de8f36b30f1e45e1f5bb880a88dba
flashnej <flashnej@gmail.com>
2020-06-08 15:23:27 -0400

    finished app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SketchPicker } from 'react-color';
+
 
 function App() {
+  const [mainColor, setMainColor] = useState('RGB(171, 86, 190)')
+  const [secondColor, setSecondColor] = useState()
+  const [thirdColor, setThirdColor] = useState()
+
+  const handleChange = (color, event) => {
+    setMainColor(`RGB(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b})`)
+    console.log(color.rgb)
+    let red = color.rgb.r
+    let green = color.rgb.g
+    let blue = color.rgb.b
+    setSecondColor(`RGB(${green}, ${blue}, ${red})`)
+    setThirdColor(`RGB(${blue}, ${red}, ${green})`)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Pick a color to get started! </h1>
+        <SketchPicker className="picker" color={mainColor} onChange={handleChange} />
+        <div className="container">
+          <div className="row">
+            <div
+              className="colorbox"
+              style={{backgroundColor: mainColor}}
+            >
+            </div>
+            <div
+              className="colorbox"
+              style={{backgroundColor: secondColor}}
+            >
+            </div>
+            <div
+              className="colorbox"
+              style={{backgroundColor: thirdColor}}
+            >
+            </div>
+          </div>
+          <div className="row">
+            <div className="colorlabel">
+              <h6>Main Color:</h6>
+            </div>
+            <div className="colorlabel">
+              <h6>Second Color:</h6>
+            </div>
+            <div className="colorlabel">
+              <h6>Third Color:</h6>
+            </div>
+          </div>
+          <div className="row">
+            <div className="colorvalue">
+              <p>{mainColor}</p>
+            </div>
+            <div className="colorvalue">
+              <p>{secondColor}</p>
+            </div>
+            <div className="colorvalue">
+              <p>{thirdColor}</p>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
