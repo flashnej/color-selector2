6c172bbe48f4ccff489e1f3fe7a22822fb533353
flashnej <flashnej@gmail.com>
2020-06-08 17:20:59 -0400

    final commit

---
+++ docs/precache-manifest.0d5315d40a9fc0442f1d00b78b8ace27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88402f12e818e7cad58d3d4fc2900ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6854f76c7a85bc25496",
+    "url": "/static/css/main.f5fee6da.chunk.css"
+  },
+  {
+    "revision": "7c01d8cf0152aca3264e",
+    "url": "/static/js/2.dc9749fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc9749fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6854f76c7a85bc25496",
+    "url": "/static/js/main.9dcd522d.chunk.js"
+  },
+  {
+    "revision": "f48cd2db2292b8ba8927",
+    "url": "/static/js/runtime-main.d36d37ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
