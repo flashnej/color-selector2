695449ef9f3117a8e58928880fea8c3d6ceac76c
flashnej <flashnej@gmail.com>
2020-06-08 17:42:29 -0400

    build

---
+++ docs/precache-manifest.2f9dbd219b0f0e0fc836755d51c44c11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a13b04621d888f3f953faa8985ce84",
+    "url": "/color-selector2/index.html"
+  },
+  {
+    "revision": "d8b360f566429f701cf6",
+    "url": "/color-selector2/static/css/main.f5fee6da.chunk.css"
+  },
+  {
+    "revision": "7c01d8cf0152aca3264e",
+    "url": "/color-selector2/static/js/2.dc9749fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-selector2/static/js/2.dc9749fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b360f566429f701cf6",
+    "url": "/color-selector2/static/js/main.3834db47.chunk.js"
+  },
+  {
+    "revision": "6496337175c61570f5a1",
+    "url": "/color-selector2/static/js/runtime-main.30124f98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-selector2/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d5315d40a9fc0442f1d00b78b8ace27.js"
+  "/color-selector2/precache-manifest.2f9dbd219b0f0e0fc836755d51c44c11.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/color-selector2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
